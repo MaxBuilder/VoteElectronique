4bc7caf6b805995b1142370a72ae8df9a2cba0be
Guerric Herve <guerric.herve@gmail.com>
2022-04-04 15:15:05 +0200

    Testing Tree structure

--- src/authorities/LocalAuthority.hpp
+++ src/authorities/LocalAuthority.hpp
@@ -1,65 +1,58 @@
 #ifndef __LOCAL_AUTHORITY_H
 #define __LOCAL_AUTHORITY_H
 
-#include "../CryptoUtils.hpp"
+#include "CryptoUtils.hpp"
 #include "Center.hpp"
 #include "RegionalAuthority.hpp"
-#include "../bulletins/LocalBulletin.hpp"
-#include "../bulletins/RegionalBulletin.hpp"
+#include "LocalBulletin.hpp"
+#include "RegionalBulletin.hpp"
 #include <boost/multiprecision/cpp_int.hpp>
 
 using namespace boost::multiprecision;
 
 /**
-* @brief Représente un centre d'autorité local.
-*/
-class LocalAuthority : public Center {
-
+ * @brief Représente un centre d'autorité local.
+ */
+class LocalAuthority : public Center
+{
 
 private:
-
     int id; // Identifiant de l'autorité locale
 
-    RegionalAuthority& regional_auth; // Référence sur l'autorité régionale dont on dépend
-
+    RegionalAuthority &regional_auth; // Référence sur l'autorité régionale dont on dépend
 
 public:
-
     // Constructeur
-    LocalAuthority(int _id, RegionalAuthority& _regional_auth) : Center(), id(_id), regional_auth(_regional_auth) {};
+    LocalAuthority(int _id, RegionalAuthority &_regional_auth) : Center(), id(_id), regional_auth(_regional_auth){};
 
     /**
-    * @brief Accesseur sur l'attribut id.
-    */
+     * @brief Accesseur sur l'attribut id.
+     */
     int get_id();
-
 
     /**
      * @brief Retourne une référence vers l'autorité régionale qui gère l'autorité locale.
-     * 
+     *
      * @return Center& une référence vers l'autorité régionale qui gère l'autorité locale.
      */
-    RegionalAuthority& get_sup_auth();
-
+    RegionalAuthority &get_sup_auth();
 
     /**
      * @brief Retourne les clés publiques de l'autorité locale et de ses deux autorités supérieures.
-     * 
+     *
      * @return std::array<PublicKey, 3> les clés publiques de l'autorité locale et de ses deux autorités supérieures.
      */
     std::array<PublicKey, 3> get_public_keys();
 
-
     /**
-    * @brief Transmet les résultats locaux à l'autorité supérieure.
-    */
+     * @brief Transmet les résultats locaux à l'autorité supérieure.
+     */
     void transmit_results() override;
 
     /**
      * @brief Affiche le BulletinBoard d'une autorité sur la sortie standard.
      */
-    void cout_board() override; 
-
+    void cout_board() override;
 
     void make_tally(cpp_int) override;
 };
--- src/authorities/NationalAuthority.hpp
+++ src/authorities/NationalAuthority.hpp
@@ -2,39 +2,33 @@
 #define __NATIONAL_AUTHORITY_H__
 
 #include "Center.hpp"
-#include "../bulletins/NationalBulletin.hpp"
+#include "NationalBulletin.hpp"
 
 /**
-* @brief Représente le centre national d'autorité.
-*/
-class NationalAuthority : public Center {
-    
+ * @brief Représente le centre national d'autorité.
+ */
+class NationalAuthority : public Center
+{
 
 public:
-
     // Constructeur
     NationalAuthority() : Center() {}
-    
 
     /**
      * @brief Annonce les résultats de l'élection.
      */
     void transmit_results() override;
 
-
     /**
      * @brief Affiche le BulletinBoard d'une autorité sur la sortie standard.
      */
     void cout_board() override;
 
-
-
     void make_tally(cpp_int) override;
 
-
     /**
-    * @brief Calcule et renvoie le vecteur du nombre de votes pour chaque candidat.
-    */
+     * @brief Calcule et renvoie le vecteur du nombre de votes pour chaque candidat.
+     */
     std::vector<cpp_int> calculate_results(cpp_int M, int p);
 };
 
