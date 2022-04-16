9a7b49045452f013c12d9fe6dd50baab6664c900
Florian Zinopoulos <florian.zinopoulos-yannopoulos@ens.uvsq.fr>
2022-04-16 14:53:51 +0200

    Ajout combinaison

--- src/Combiner.cpp
+++ src/Combiner.cpp
@@ -3,6 +3,7 @@
 
 Combiner::Combiner(cpp_int sk_, cpp_int delta_, cpp_int modulus, int nb_servers, cpp_int m) {
 	sk = sk_;
+	delta = delta_;
 
 	std::vector<cpp_int> secret_shares = generateSecretShares(nb_servers, modulus, m);
 
@@ -46,5 +47,70 @@ std::vector<cpp_int> Combiner::generateSecretShares(int nb_servers, cpp_int N, c
 	for (int i = 1; i <= nb_servers; i++)
 		res.push_back(boost::multiprecision::powm(horner(ai, i), 1, Nm));
 
+	return res;
+}
+
+
+std::vector<cpp_int> Combiner::getResults(cpp_int c)
+{
+	for (CryptoServer* server : servers)
+	{
+		cpp_int res = server->computeDecryptionShare(c);
+		results.push_back(res);
+	}
+}
+
+
+cpp_int Combiner::calculateMu(int j)
+{
+	cpp_int res;
+	double res_tmp = 1.;
+	int nb_servers = servers.size();
+	
+	for (int j_prime = 0; j_prime < nb_servers; j_prime++)
+	{
+		if (j_prime == j)
+			continue;
+
+		res_tmp = res_tmp * (j_prime / (j_prime - j));
+	}
+
+	long long delta_ = (long long)delta; // En pratique delta devrait mesurer moins de 128 bits
+	res = cpp_int(delta_ * res_tmp);
+	return res;
+}
+
+
+cpp_int Combiner::Lfunction(cpp_int u, cpp_int N)
+{
+	cpp_int res;
+	subtract(res, u, cpp_int(1));
+	return res / N;
+}
+
+
+cpp_int Combiner::combine()
+{
+	cpp_int res = 1;
+	cpp_int N2;
+	multiply(N2, pk.N, pk.N);
+
+	for (int j = 0; j < servers.size(); j++)
+	{
+		cpp_int power;
+		cpp_int mu = calculateMu(j);
+		multiply(power, mu, cpp_int(2));
+		cpp_int tmp = powm(results[j], power, N2);
+		multiply(res, res, tmp);
+	}
+
+	res = Lfunction(res, pk.N);
+
+	cpp_int divider, delta2;
+	multiply(divider, cpp_int(4), pk.tetha);
+	multiply(delta2, delta, delta);
+	multiply(divider, divider, delta2);
+
+	res = res / divider;
 	return res;
 }
--- src/Combiner.hpp
+++ src/Combiner.hpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <array>
 #include <boost/random.hpp>
+#include <boost/multiprecision/cpp_bin_float.hpp>
 
 #include "CryptoServer.hpp"
 #include "CryptoUtils.hpp"
@@ -17,11 +18,15 @@ private:
 
 	cpp_int sk; // La clé secrète de l'autorité.
 
+	cpp_int delta; // Le paramètre delta (nombre de serveurs factoriel) 
+
 	PublicKey pk; // La clé publique de l'autorité. Nécessaire pour obtenir les paramètres dans les méthodes suivantes.
+
+	std::vector<cpp_int> results; // Vecteur qui contient les déchiffrements partiels provenant des CryptoServers. 
 
 
 	/**
-	* @brief Utilisee dans le constructeur du combiner afin de donner les "secrets shares" de la cle privee aux serveurs.
+	* @brief Utilisée dans le constructeur du combiner afin de donner les "secrets shares" de la cle privee aux serveurs.
 	*/
 	std::vector<cpp_int> generateSecretShares(int nb_servers, cpp_int N, cpp_int m);
 
@@ -33,11 +38,30 @@ private:
 	*/
 	cpp_int horner(std::vector<cpp_int> poly, cpp_int x);
 
+	/*
+	* @Brief Calcul de l'exposant mu_0 pour l'algo de combinaison
+	*/
+	cpp_int calculateMu(int j);
+
+	/*
+	* La fonction L du cryptosystème de Paillier.
+	*/
+	cpp_int Lfunction(cpp_int u, cpp_int N);
 
 public:
 
 	// Constructeur
 	Combiner(cpp_int sk_, cpp_int delta_, cpp_int modulus, int nb_servers, cpp_int m);
+
+	/*
+	* @brief Récupère les déchiffrements partiels.
+	*/
+	std::vector<cpp_int> getResults(cpp_int c);
+
+	/*
+	* @Brief La combinason des déchiffrés partiels.
+	*/
+	cpp_int combine();
 
 };
 
