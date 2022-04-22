d545ff186bb89100de685efcba3c8bc79037360b
vdElyn <vdelyn@protonmail.com>
2022-04-22 17:54:10 +0200

    Calcul de Mu avec j allant de 1 à N et inversion de mu si négatif (problème: n'est pas toujours inversible)

--- src/Combiner.cpp
+++ src/Combiner.cpp
@@ -61,25 +61,21 @@ void Combiner::calculateResults(cpp_int c)
 
 cpp_int Combiner::calculateMu(int j)
 {
-	std::cout << "\ncalculateMu func\n";
 	cpp_int res;
 	double res_tmp = 1.;
 	int nb_servers = servers.size();
 
-	// ToDo: passer les j de 1 à nb_servers+1 
-	for (int j_prime = 0; j_prime < nb_servers; j_prime++)
+	j++; // On considère les serveurs numérotés de 1 à N dans le calcul de Mu
+	for (int j_prime = 1; j_prime < nb_servers+1; j_prime++)
 	{
 		if (j_prime == j)
 			continue;
-		std::cout << "j: " << j << ", j_prime: " << j_prime << ", j_prime-j: " << j_prime-j << "\n";
 		res_tmp = res_tmp * (j_prime / (j_prime - j));
 	}
 	// En pratique delta devrait mesurer moins de 128 bits
-	std::cout << "calculateMu out of loop\n delta: " << delta << "\n res_tmp: " << res_tmp << "\n";
 	
 	multiply(res, (cpp_int) res_tmp, delta);
 	
-	std::cout << "calculateMu end, res: " << res << "\n";
 	return res;
 }
 
@@ -95,34 +91,61 @@ cpp_int Combiner::Lfunction(cpp_int u, cpp_int N)
 cpp_int Combiner::combine()
 {
 	// ToDo: vérifier d'abord si t decryption shares sont valides
-	std::cout << "Combine func\n";
+
+	std::cout << "\n > Combine func:\n";
 	cpp_int res = 1;
 	cpp_int N2;
 	multiply(N2, pk.N, pk.N);
 
-	cpp_int power;
+	cpp_int base;
+	cpp_int exponent;
 	for (size_t j = 0; j < servers.size(); j++)
 	{
 		std::cout << "\n   Serveur " << j << ":\n";
 		cpp_int mu = calculateMu(j);
 		std::cout << "mu: " << mu << "\n";
 
-		multiply(power, mu, cpp_int(2)); // mu*2
+		multiply(exponent, mu, cpp_int(2)); // exponent: mu*2
+		base = results[j];
 		std::cout << "c_j: " << results[j] << ", N²: " << N2 <<"\n";
-		// ToDo: calcul du powm avec mu négatif
-		cpp_int tmp = powm(results[j], power, N2); // c_j ^ 2*mu
-		std::cout << "c_j ^ 2*mu: " << tmp  << "\n";
+		if (exponent < 0) {
+			// Si l'exposant est négatif, on calcule l'inverse modulaire en N2 de la base et on passe l'exposant positif
+			std::cout << "Exposant négatif: on inverse la base " << base << " modulo " << N2 << "\n";
+			base = boost::integer::mod_inverse(base, N2);
+			if (base == 0)  {
+				std::cout << "There is no inverse of base mod " <<N2<<"\n";
+				return -1;
+			}
+			exponent = -exponent;
+		}
+		cpp_int tmp = powm(base, exponent, N2); // c_j ^ 2*mu
+		std::cout << "c_j ^ 2*mu mod N²: " << tmp  << "\n";
 		multiply(res, res, tmp);
 	}
-	std::cout << "Before LFunc:\n res:" << res << "\n";
+	std::cout << "\n   Before LFunc: res:" << res << "\n";
 	res = Lfunction(res, pk.N);
+	std::cout << "   After LFunc: res:" << res << "\n\n";
 
 	cpp_int divider, delta2;
-	multiply(divider, cpp_int(4), pk.tetha);
-	multiply(delta2, delta, delta);
-	multiply(divider, divider, delta2);
+	cpp_int invDivider;
+	multiply(divider, cpp_int(4), pk.tetha); // 4*Tetha
+	multiply(delta2, delta, delta); // Delta^2
+	multiply(divider, divider, delta2); // 4*Tetha*Delta^2
 	
-	std::cout << "divider: " << divider << "\n";
-	res = res / divider;
+	std::cout << "tetha: " << pk.tetha << "\n";
+	std::cout << "divider (4*Tetha*Delta^2): " << divider << "\n";
+
+	// ToDo: inverse modulaire de 4*Tetha*Delta^2 
+	//		(/!\ l'inverse donne 0 actuellement !!)
+	divider = powm(divider, 1, pk.N); // divider mod N
+	invDivider = boost::integer::mod_inverse(divider, pk.N);
+	if (invDivider == 0)  {
+		std::cout << "There is no inverse of "<< divider << " mod " <<pk.N<<"\n\n";
+		return -1;
+	}
+
+	std::cout << "inverse de (4*Tetha*Delta^2) mod N: " << invDivider << "\n\n";
+	multiply(res, res, invDivider);
+	std::cout << "Résultat de combinaison: " << res << "\n";
 	return res;
 }
